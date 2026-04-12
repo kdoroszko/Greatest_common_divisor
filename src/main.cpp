f4950641766f277e3e98adf6f167214f03f9492a
Krzysztof Doroszko <doroszko.k@gmail.com>
2026-04-12 16:18:33 +0200

    main function is implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
+#include "functions.hpp"
+
 int main()
 {
+    int firstNumber, secondNumber;
+
+    std::cout << "Enter two natural numbers: ";
+    std::cin >> firstNumber >> secondNumber;
+
+    std::unordered_multiset<int> firstNumberDivisors = PrimeFactorization(firstNumber);
+    std::unordered_multiset<int> secondNumberDivisors = PrimeFactorization(secondNumber);
+
+    std::unordered_multiset<int> commonDivisors = FindCommonDivisors(firstNumberDivisors, secondNumberDivisors);
+
+    int greatestCommonDivisor = FindGreatestCommonDivisor(commonDivisors);
+
+    std::cout << "The greatest common divisor of " << firstNumber << " and " << secondNumber << " is: " << greatestCommonDivisor << '\n';
+
     return 0;
 }
